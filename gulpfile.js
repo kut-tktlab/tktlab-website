565781fd66f2159a9bd264d9de5bad16815e0d2a
nyamadori <nyamadorig@gmail.com>
2015-12-31 22:48:38 +0900

    Add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('webserver', function () {
+  gulp.src('public')
+    .pipe(plugins.webserver({
+      root: './public',
+      livereload: true,
+      host: '0.0.0.0'
+    }));
+});
+
+gulp.task('html', function () {
+  gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('stylesheets', function () {
+  gulp.src('./src/stylesheets/**/*.css')
+    .pipe(gulp.dest('./public/stylesheets'));
+});
+
+gulp.task('images', function () {
+  gulp.src('./src/images/**/*.{png,jpg,jpeg,svg}')
+    .pipe(gulp.dest('./public/images'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/**/*.html'], ['html']);
+  gulp.watch(['./src/stylesheets/**/*.css'], ['stylesheets']);
+  gulp.watch(['./src/images/**/*.{png,jpg,jpeg,svg}'], ['images']);
+});
+
+gulp.task('default', ['html', 'stylesheets', 'images', 'stylesheets', 'webserver', 'watch']);
